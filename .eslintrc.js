702bea15870287d2f7d14529facb325164af8cee
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2022-09-24 22:42:10 +0300

    _v2w

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
-// module.exports = {
-//     "env": {
-//         "browser": true,
-//         "es2021": true
-//     },
-//     "extends": "plugin:react/recommended",
-//     "overrides": [
-//     ],
-//     "parserOptions": {
-//         "ecmaVersion": "latest"
-//     },
-//     "plugins": [
-//         "react"
-//     ],
-//     "rules": {
-//     }
-// }
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "plugin:react/recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
