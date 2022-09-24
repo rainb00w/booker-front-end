964beb1222d3dbe6ee99da3a4dbd5b601b9765af
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2022-09-24 23:11:17 +0300

    _v2p

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "plugin:react/recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
