56c9433cf33314f71450dd0b549797855d5b6a4c
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2022-09-24 22:43:15 +0300

    _V2j

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "es2021": true
+        "commonjs": true,
+        "es2021": true,
+        "node": true
     },
     "extends": "plugin:react/recommended",
     "overrides": [
