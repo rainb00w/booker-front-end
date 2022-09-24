7f2f0d2601a4dc2c76e45d6664c4c2c8a6b60697
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2022-09-24 23:21:02 +0300

    _v2a

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,9 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "plugin:react/recommended",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    {
+        "env": { "es2021": true },
+        "plugins": ["decorator-position"],
+        "rules": {
+          "decorator-position/decorator-position": "error"
+        }
+      }
 }
