4c8fe1afaba1d8bfb26a7897739f71a64487d0e9
jokerav <jokeravdeveloper@gmail.com>
2022-09-25 12:48:18 +0300

    начал секцию

--- src/pages/statistics/statistics.js
+++ src/pages/statistics/statistics.js
@@ -1,12 +1,17 @@
-import React  from 'react';
+import React from 'react';
+import FormPropsTextFields from './inputPage';
+import BasicTable from './statisticsTable.js';
 
 const Statistics = () => {
-    return (
-      <>
-        <p>Statistics</p>
-      </>
-    );
-  };
-  
-  export default Statistics;
-  
+  return (
+    <>
+      <h2>Результати</h2>
+      <FormPropsTextFields />
+      <button type="submit">Додати результат</button>
+      <h2>Статистика</h2>
+      <BasicTable />
+    </>
+  );
+};
+
+export default Statistics;
---
+++ src/pages/statistics/statisticsTable.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(date, time, page) {
+  return { date, time, page };
+}
+
+const rows = [
+  createData('10.10.2019', '08:08:00', 32),
+  createData('10.10.2019', '08:08:00', 154),
+  createData('10.10.2019', '08:08:00', 6),
+  createData('10.10.2019', '08:08:00', 17),
+  createData('10.10.2019', '08:08:00', 18),
+];
+
+export default function BasicTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ width: 288 }} aria-label="simple table">
+        <TableHead>
+          <TableRow></TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow
+              key={row.date}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.date}
+              </TableCell>
+              <TableCell align="right">{row.time}</TableCell>
+              <TableCell align="right">{row.page}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
