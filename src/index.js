57047da2d1ee34d8674adf0397d4e53c697e6fe0
Serhii Rubtsov <rubtsov1986@bigmir.net>
2022-09-25 16:02:41 +0300

    change BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import '/node_modules/modern-normalize/modern-normalize.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/booker-front-end">
+    <HashRouter basename="/booker-front-end">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
