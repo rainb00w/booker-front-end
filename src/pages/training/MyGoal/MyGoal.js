9832071795babda2e6453deadd54817748ce287e
Vitalii Peresoliak <vitalik21022@gmail.com>
2022-09-25 17:24:23 +0300

    added my goal section

---
+++ src/pages/training/MyGoal/MyGoal.js
@@ -0,0 +1,25 @@
+import s from './MyGoal.module.css';
+
+const MyGoal = () => {
+    return (
+        <div>
+            <h2>Моя мета прочитати</h2>
+            <div>
+                <div>
+                    <span>0</span>
+                </div>
+                <div>
+                    <span>0</span>
+                </div>
+                <div>
+                    <p>Кількість книжок</p>
+                    <p>Кількість днів</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default MyGoal;
