8c1914fcb8ee95239659d965dccca5a114c427ad
puerarongo <akhardin96@gmail.com>
2022-09-26 16:05:09 +0300

    structure update

---
+++ src/pages/registration/registrationPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Registration from "../../components/pages/registration/registration";
+
+const RegistrationPage = () => {
+    return (
+        <>
+            <Registration />
+        </>
+    )
+};
+
+export default RegistrationPage;
