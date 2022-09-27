e9d0c51c4c080ee6bcbf797a72877064e9ec8e3a
ValeriiaMalyshko <91088527+ValeriiaMalyshko@users.noreply.github.com>
2022-09-28 00:21:41 +0300

    library table

--- src/pages/library/library.js
+++ src/pages/library/library.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import BookAddForm from './bookAddForm';
-import BookTable from './bookTable';
+import BookAddForm from '../../components/bookAdd/bookAddForm';
+import BookTable from '../../components/bookTable/bookTable';
+import BookTableMobile from 'components/bookTable/bookMobileTable';
+import BtnMyTraining from 'components/BtnLibrary/btnMyTraining';
+import { useMediaQuery } from 'react-responsive';
 
 const Library = () => {
+  const mobile = useMediaQuery({ query: '(max-width: 767px)' });
   return (
     <>
       <h2>Library Section -------- </h2>
       <BookAddForm />
-      <BookTable text="Already read" />
-      <BookTable text="Reading now" />
-      <BookTable text="Going to read" />
+      {mobile ? (
+        <BookTableMobile text="Already read" />
+      ) : (
+        <BookTable text="Already read" />
+      )}
+      {mobile ? (
+        <BookTableMobile text="Reading now" />
+      ) : (
+        <BookTable text="Reading now" />
+      )}
+      {mobile ? (
+        <BookTableMobile text="Going to read" />
+      ) : (
+        <BookTable text="Going to read" />
+      )}
+      <BtnMyTraining />
     </>
   );
 };
