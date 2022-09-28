4519fae26c4ff2ebeee7aa9fcec899290d7f3c76
jokerav <jokeravdeveloper@gmail.com>
2022-09-29 00:00:31 +0300

    Модалка "Ти молодчина"

--- src/pages/statistics/statistics.js
+++ src/pages/statistics/statistics.js
@@ -1,28 +1,33 @@
 import React from 'react';
-import FormPropsTextFields from './inputPage';
+
 import StatisticsList from './statisticsList';
 import s from './statisticsList.module.css';
 
+import ModalYouAreGood from './modal/ModalYouAreGood';
+
 const Statistics = () => {
   return (
-    <div className={s.container}>
-      <h2 className={s.resultsHeader}>Результати</h2>
-      <div className={s.inputs}>
-        <label className={s.inputsLabel}>
-          Дата
-          <input className={s.inputPage} type="date" name="dateInput" />
-        </label>
-        <label className={s.inputsLabel}>
-          Кількість сторінок
-          <input className={s.inputDate} type="number" name="pageInput" />
-        </label>
+    <>
+      <div className={s.container}>
+        <h2 className={s.resultsHeader}>Результати</h2>
+        <div className={s.inputs}>
+          <label className={s.inputsLabel}>
+            Дата
+            <input className={s.inputPage} type="date" name="dateInput" />
+          </label>
+          <label className={s.inputsLabel}>
+            Кількість сторінок
+            <input className={s.inputDate} type="number" name="pageInput" />
+          </label>
+        </div>
+        <button className={s.addResultBtn} type="submit">
+          Додати результат
+        </button>
+        <h2 className={s.statisticsHeader}>Статистика</h2>
+        <StatisticsList />
       </div>
-      <button className={s.addResultBtn} type="submit">
-        Додати результат
-      </button>
-      <h2 className={s.statisticsHeader}>Статистика</h2>
-      <StatisticsList />
-    </div>
+      <ModalYouAreGood />
+    </>
   );
 };
 
