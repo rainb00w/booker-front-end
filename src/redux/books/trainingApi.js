2cff789fc751e0234a09e97bd487a32184adb1fc
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2022-09-29 19:31:41 +0300

    _v3

---
+++ src/redux/books/trainingApi.js
@@ -0,0 +1,48 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const trainingApi = createApi({
+  reducerPath: 'training',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:3001/api/training',
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth.token;
+      // console.log('TOKEN', token);
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+  }),
+  refetchOnMountOrArgChange: true,
+  keepUnusedDataFor: 1,
+  tagTypes: ['Training'],
+  endpoints: builder => ({
+    getAllTrainings: builder.query({
+      query: () => `/`,
+      method: 'GET',
+      providesTags: ['Training'],
+    }),
+    addTraining: builder.mutation({
+      query: ({startDate, finishDate, books }) => ({
+        url: '/',
+        method: 'POST',
+        body: {startDate, finishDate, books },
+      }),
+      invalidatesTags: ['Training'],
+    }),
+    deleteTraining: builder.mutation({
+      query: id => ({
+        url: `/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Training'],
+    }),
+  }),
+});
+
+export const {
+  useGetAllTrainingsQuery,
+  useAddTrainingMutation,
+  useDeleteTrainingMutation,
+} = trainingApi;
