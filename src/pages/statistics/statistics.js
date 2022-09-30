0d6cd573eb313f08ed2d22e9fdb8c523e12df145
jokerav <jokeravdeveloper@gmail.com>
2022-09-30 23:57:38 +0300

    Update statistics.js

--- src/pages/statistics/statistics.js
+++ src/pages/statistics/statistics.js
@@ -3,11 +3,15 @@ import SendPageForm from './sendPageForm';
 import StatisticsList from './statisticsList';
 import s from './statisticsList.module.css';
 
+import { useGetAllTrainingsQuery } from '../../redux/books/trainingApi';
+
 import ModalYouAreGood from './modal/ModalYouAreGood';
 import ModalCongratulate from './modal/ModalCongratulate';
 import ModalExit from './modal/ModalExit';
 
 const Statistics = () => {
+  const training = useGetAllTrainingsQuery();
+  // console.log(training);
   return (
     <>
       <div className={s.container}>
