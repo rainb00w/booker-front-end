16b7c56802307ebacf587b99754ef150ee3fe3a8
puerarongo <akhardin96@gmail.com>
2022-10-01 15:27:34 +0300

    add

--- src/services/yupValidationSchema.js
+++ src/services/yupValidationSchema.js
@@ -2,30 +2,31 @@ import * as yup from 'yup';
 
 
 export const registrationValidationSchema = yup.object().shape({
-        name: yup.string()
-            .typeError("Will be a string")
-            .min(3)
-            .max(100)
-            .required("Required field"),
-        email: yup.string()
-            .typeError("Will be a string")
-            .email()
-            .matches(/^(([^<>()\[\]\\.,;:\s@!?"]{2,}(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ , 'Is not in correct format')
-            .min(10)
-            .max(63)
-            .required("Required field"),
-        password: yup.string()
-            .typeError("Will be a string")
-            .min(5)
-            .max(30)
-           // .matches(/^[^<>()\[\]\\.,;:\s"]*/, 'Is not in correct format')
-            .required("Required field"),
+    name: yup.string()
+        .typeError("Will be a string")
+        .min(3)
+        .max(100)
+        .required("Required field"),
+    email: yup.string()
+        .typeError("Will be a string")
+        .email()
+        .matches(/^(([^<>()\[\]\\.,;:\s@!?"]{2,}(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ , 'Is not in correct format')
+        .min(10)
+        .max(63)
+        .required("Required field"),
+    password: yup.string()
+        .typeError("Will be a string")
+        .min(5)
+        .max(30)
+        .matches(/^(?!\-|\.)[0-9a-zA-Z\*\!\@\#\$\%\^\&\(\)\{\}\[\]\:\;\<\>,\.\?\/\~_\+\-\=\|\\]{5,30}$/, 'Is not in correct format')
+        .required("Required field"),
     confirmPassword: yup.string()
-            .typeError("Will be a string")
-            .min(5)
-            .max(30)
-            .oneOf([yup.ref('password')], "Passwords doesn't match")
-            .required("Required field")
+        .typeError("Will be a string")
+        .min(5)
+        .max(30)
+        .matches(/^(?!\-|\.)[0-9a-zA-Z\*\!\@\#\$\%\^\&\(\)\{\}\[\]\:\;\<\>,\.\?\/\~_\+\-\=\|\\]{5,30}$/, 'Is not in correct format')
+        .oneOf([yup.ref('password')], "Passwords doesn't match")
+        .required("Required field")
 });
 
 
@@ -41,7 +42,7 @@ export const loginValidationSchema = yup.object().shape({
         .typeError("Will be a string")
         .min(5)
         .max(30)
-        // .matches(/^[^<>()\[\]\\.,;:\s"]*/, 'Is not in correct format')
+        .matches(/^(?!\-|\.)[0-9a-zA-Z\*\!\@\#\$\%\^\&\(\)\{\}\[\]\:\;\<\>,\.\?\/\~_\+\-\=\|\\]{5,30}$/, 'Is not in correct format')
         .required("Required field"),
 });
 
