a2e2a6432ae2430fc0bb688deab2a49aec7c3851
jokerav <jokeravdeveloper@gmail.com>
2022-10-01 14:17:58 +0300

    валидация даты начала тренировки

--- src/pages/statistics/statistics.js
+++ src/pages/statistics/statistics.js
@@ -3,15 +3,11 @@ import SendPageForm from './sendPageForm';
 import StatisticsList from './statisticsList';
 import s from './statisticsList.module.css';
 
-import { useGetAllTrainingsQuery } from '../../redux/books/trainingApi';
-
 import ModalYouAreGood from './modal/ModalYouAreGood';
 import ModalCongratulate from './modal/ModalCongratulate';
 import ModalExit from './modal/ModalExit';
 
 const Statistics = () => {
-  // const [getAllTrainings] = useGetAllTrainingsQuery();
-  // console.log(getAllTrainings);
   return (
     <>
       <div className={s.container}>
