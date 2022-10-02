f813703ab15cb1963c452e973a58ff72e0596162
ValeriiaMalyshko <91088527+ValeriiaMalyshko@users.noreply.github.com>
2022-10-02 17:46:32 +0300

    library-page

--- src/components/bookTable/bookTable.js
+++ src/components/bookTable/bookTable.js
@@ -11,149 +11,162 @@ export default function BookTable() {
   const { data } = useGetAllBooksQuery();
   const [deleteContact, { isLoading: isDeleting }] = useDeleteBookMutation();
 
+  const status = e => {
+    const status = data?.payload.books.some(book => book.status === e);
+    return status;
+  };
+
   return (
     <>
       <section className={s.section}>
-        <div className={s.table}>
-          <h3 className={s.title}>Already read</h3>
-          <ul className={s.head}>
-            <li className={s.topic}>Book title</li>
-            <li className={s.topic}>Author</li>
-            <li className={s.topic}>Year</li>
-            <li className={s.topic}>Pages</li>
-            <li className={s.topic}>Rating</li>
-          </ul>
-          <ul className={s.body}>
-            {data?.payload.books.map(
-              ({ _id, author, pages, title, year, status }) =>
-                status === 'haveRead' && (
-                  <li key={_id} className={s.item}>
-                    <p className={s.subtitle}>
-                      <svg width={22} height={17} className={s.img}>
-                        <use href={`${star}#white_book`}></use>
-                      </svg>
-                      {title}
-                    </p>
-                    <p className={s.subtitle}> {author}</p>
-                    <p className={s.subtitle}>{year}</p>
-                    <p className={s.subtitle}>{pages}</p>
-                    <p className={s.subtitle}>
-                      {rating >= 1 ? (
-                        <svg width={17} height={17}>
-                          <use href={`${star}#yellow_star`}></use>
+        {status('haveRead') && (
+          <div className={s.table}>
+            <h3 className={s.title}>Already read</h3>
+            <ul className={s.head}>
+              <li className={s.topic}>Book title</li>
+              <li className={s.topic}>Author</li>
+              <li className={s.topic}>Year</li>
+              <li className={s.topic}>Pages</li>
+              <li className={s.topic}>Rating</li>
+            </ul>
+            <ul className={s.body}>
+              {data?.payload.books.map(
+                ({ _id, author, pages, title, year, status }) =>
+                  status === 'haveRead' && (
+                    <li key={_id} className={s.item}>
+                      <p className={s.subtitle}>
+                        <svg width={22} height={17} className={s.img}>
+                          <use href={`${star}#white_book`}></use>
                         </svg>
-                      ) : (
-                        <svg width={17} height={17}>
-                          <use href={`${star}#white_star`}></use>
+                        {title}
+                      </p>
+                      <p className={s.subtitle}> {author}</p>
+                      <p className={s.subtitle}>{year}</p>
+                      <p className={s.subtitle}>{pages}</p>
+                      <p className={s.subtitle}>
+                        {rating >= 1 ? (
+                          <svg width={17} height={17}>
+                            <use href={`${star}#yellow_star`}></use>
+                          </svg>
+                        ) : (
+                          <svg width={17} height={17}>
+                            <use href={`${star}#white_star`}></use>
+                          </svg>
+                        )}
+                        {rating >= 2 ? (
+                          <svg width={17} height={17}>
+                            <use href={`${star}#yellow_star`}></use>
+                          </svg>
+                        ) : (
+                          <svg width={17} height={17}>
+                            <use href={`${star}#white_star`}></use>
+                          </svg>
+                        )}
+                        {rating >= 3 ? (
+                          <svg width={17} height={17}>
+                            <use href={`${star}#yellow_star`}></use>
+                          </svg>
+                        ) : (
+                          <svg width={17} height={17}>
+                            <use href={`${star}#white_star`}></use>
+                          </svg>
+                        )}
+                        {rating >= 4 ? (
+                          <svg width={17} height={17}>
+                            <use href={`${star}#yellow_star`}></use>
+                          </svg>
+                        ) : (
+                          <svg width={17} height={17}>
+                            <use href={`${star}#white_star`}></use>
+                          </svg>
+                        )}
+                        {rating >= 5 ? (
+                          <svg width={17} height={17}>
+                            <use href={`${star}#yellow_star`}></use>
+                          </svg>
+                        ) : (
+                          <svg width={17} height={17}>
+                            <use href={`${star}#white_star`}></use>
+                          </svg>
+                        )}
+                        <button type="button" className={s.btn}>
+                          Resume
+                        </button>
+                      </p>
+                    </li>
+                  )
+              )}
+            </ul>
+          </div>
+        )}
+        {status('reading') && (
+          <div className={s.table}>
+            <h3 className={s.title}>Reading now</h3>
+            <ul className={s.head}>
+              <li className={s.topic}>Book title</li>
+              <li className={s.topic}>Author</li>
+              <li className={s.topic}>Year</li>
+              <li className={s.topic}>Pages</li>
+            </ul>
+            <ul className={s.body}>
+              {data?.payload.books.map(
+                ({ _id, author, pages, title, year, status }) =>
+                  status === 'reading' && (
+                    <li key={_id} className={s.item}>
+                      <p className={s.subtitle}>
+                        <svg width={22} height={17} className={s.img}>
+                          <use href={`${star}#yellow_book`}></use>
                         </svg>
-                      )}
-                      {rating >= 2 ? (
-                        <svg width={17} height={17}>
-                          <use href={`${star}#yellow_star`}></use>
+                        {title}
+                      </p>
+                      <p className={s.subtitle}> {author}</p>
+                      <p className={s.subtitle}>{year}</p>
+                      <p className={s.subtitle}>{pages}</p>
+                    </li>
+                  )
+              )}
+            </ul>
+          </div>
+        )}
+        {status('toRead') && (
+          <div className={s.table}>
+            <h3 className={s.title}>Going to read</h3>
+            <ul className={s.head}>
+              <li className={s.topic}>Book title</li>
+              <li className={s.topic}>Author</li>
+              <li className={s.topic}>Year</li>
+              <li className={s.topic}>Pages</li>
+            </ul>
+            <ul className={s.body}>
+              {data?.payload.books.map(
+                ({ _id, author, pages, title, year, status }) =>
+                  status === 'toRead' && (
+                    <li key={_id} className={s.item}>
+                      <p className={s.subtitle}>
+                        <svg width={22} height={17} className={s.img}>
+                          <use href={`${star}#white_book`}></use>
                         </svg>
-                      ) : (
-                        <svg width={17} height={17}>
-                          <use href={`${star}#white_star`}></use>
-                        </svg>
-                      )}
-                      {rating >= 3 ? (
-                        <svg width={17} height={17}>
-                          <use href={`${star}#yellow_star`}></use>
-                        </svg>
-                      ) : (
-                        <svg width={17} height={17}>
-                          <use href={`${star}#white_star`}></use>
-                        </svg>
-                      )}
-                      {rating >= 4 ? (
-                        <svg width={17} height={17}>
-                          <use href={`${star}#yellow_star`}></use>
-                        </svg>
-                      ) : (
-                        <svg width={17} height={17}>
-                          <use href={`${star}#white_star`}></use>
-                        </svg>
-                      )}
-                      {rating >= 5 ? (
-                        <svg width={17} height={17}>
-                          <use href={`${star}#yellow_star`}></use>
-                        </svg>
-                      ) : (
-                        <svg width={17} height={17}>
-                          <use href={`${star}#white_star`}></use>
-                        </svg>
-                      )}
-                      <button type="button" className={s.btn}>
-                        Resume
-                      </button>
-                    </p>
-                  </li>
-                )
-            )}
-          </ul>
-        </div>
-        <div className={s.table}>
-          <h3 className={s.title}>Reading now</h3>
-          <ul className={s.head}>
-            <li className={s.topic}>Book title</li>
-            <li className={s.topic}>Author</li>
-            <li className={s.topic}>Year</li>
-            <li className={s.topic}>Pages</li>
-          </ul>
-          <ul className={s.body}>
-            {data?.payload.books.map(
-              ({ _id, author, pages, title, year, status }) =>
-                status === 'reading' && (
-                  <li key={_id} className={s.item}>
-                    <p className={s.subtitle}>
-                      <svg width={22} height={17} className={s.img}>
-                        <use href={`${star}#yellow_book`}></use>
-                      </svg>
-                      {title}
-                    </p>
-                    <p className={s.subtitle}> {author}</p>
-                    <p className={s.subtitle}>{year}</p>
-                    <p className={s.subtitle}>{pages}</p>
-                  </li>
-                )
-            )}
-          </ul>
-        </div>
-        <div className={s.table}>
-          <h3 className={s.title}>Going to read</h3>
-          <ul className={s.head}>
-            <li className={s.topic}>Book title</li>
-            <li className={s.topic}>Author</li>
-            <li className={s.topic}>Year</li>
-            <li className={s.topic}>Pages</li>
-          </ul>
-          <ul className={s.body}>
-            {data?.payload.books.map(
-              ({ _id, author, pages, title, year, status }) =>
-                status === 'toRead' && (
-                  <li key={_id} className={s.item}>
-                    <p className={s.subtitle}>
-                      <svg width={22} height={17} className={s.img}>
-                        <use href={`${star}#white_book`}></use>
-                      </svg>
-                      {title}
-                    </p>
-                    <p className={s.subtitle}>{author}</p>
-                    <p className={s.subtitle}>{year}</p>
-                    <p className={s.subtitle}>{pages}</p>
-                    <button
-                      type="button"
-                      className={s.btn}
-                      onClick={() => deleteContact(_id)}
-                      disabled={isDeleting}
-                    >
-                      Delete Book
-                    </button>
-                  </li>
-                )
-            )}
-          </ul>
-        </div>
+                        {title}
+                      </p>
+                      <p className={s.subtitle}>{author}</p>
+                      <p className={s.subtitle}>{year}</p>
+                      <p className={s.subtitle}>
+                        {pages}{' '}
+                        <button
+                          type="button"
+                          className={s.btn}
+                          onClick={() => deleteContact(_id)}
+                          disabled={isDeleting}
+                        >
+                          Delete Book
+                        </button>
+                      </p>
+                    </li>
+                  )
+              )}
+            </ul>
+          </div>
+        )}
       </section>
     </>
   );
--- src/pages/library/library.js
+++ src/pages/library/library.js
@@ -5,16 +5,26 @@ import BookTableMobile from 'components/bookTable/bookMobileTable';
 import BtnMyTraining from 'components/BtnLibrary/btnMyTraining';
 import { useMediaQuery } from 'react-responsive';
 import s from './library.module.css';
+import { useGetAllBooksQuery } from 'redux/books/booksApi';
+import Info from '../../components/EmptyLibrary/EmptyLibrary';
 
 const Library = () => {
   const mobile = useMediaQuery({ query: '(max-width: 767px)' });
+  const { data } = useGetAllBooksQuery();
   return (
     <>
-      <div className={s.section}>
-        <BookAddForm />
-        {mobile ? <BookTableMobile /> : <BookTable />}
-        <BtnMyTraining />
-      </div>
+      {!data?.payload.books.length > 0 ? (
+        <div className={s.section}>
+          <BookAddForm />
+          <Info />
+        </div>
+      ) : (
+        <div className={s.section}>
+          <BookAddForm />
+          {mobile ? <BookTableMobile /> : <BookTable />}
+          <BtnMyTraining />
+        </div>
+      )}
     </>
   );
 };
