c00a9d3307b9726de656efe34cfc4d565f35a03f
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2022-10-04 15:44:19 +0300

    _v7r

--- src/redux/books/booksApi.js
+++ src/redux/books/booksApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const booksApi = createApi({
   reducerPath: 'books',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3001/api/books',
+    baseUrl: 'https://booker-back-end.herokuapp.com/api/books',
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
 
