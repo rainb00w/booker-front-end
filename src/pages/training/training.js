f6a5cc55e027fe8d9b4dc35be9384f6d255e1734
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2022-10-04 22:52:42 +0300

    _v7l

--- src/pages/training/training.js
+++ src/pages/training/training.js
@@ -91,6 +91,7 @@ const Training = () => {
   const sendToStatisticResults = trainingData?.data?.results
   // console.log('startDate', trainingData.data.startDate ,'array', trainingData.data.results );
 
+
   let isEmptyTraining = false;
   if (trainingData?.data === undefined) {
     isEmptyTraining = true;
