16f5201b2b043853e635b306867b627b11ff2395
puerarongo <akhardin96@gmail.com>
2022-10-04 19:05:45 +0300

    add

--- src/services/newPasswordAPI.js
+++ src/services/newPasswordAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const PATH = 'http://localhost:3001/api/user/forgotPassword'; 
-// const PATH = 'https://booker-back-end.herokuapp.com/api/user/forgotPassword'
+// const PATH = 'http://localhost:3001/api/user/forgotPassword'; 
+const PATH = 'https://booker-back-end.herokuapp.com/api/user/forgotPassword'
 
 const newPasswordAPI = async (email, password) => {
     const data = await axios.patch(PATH, { email, password })
