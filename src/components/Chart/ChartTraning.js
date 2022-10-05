b56db1d9aec2571cef2c2e7f688c31298f146e25
Lilia Lomaka <llomaka80@gmail.com>
2022-10-05 17:23:57 +0300

    Cleaning Chart component

--- src/components/Chart/ChartTraning.js
+++ src/components/Chart/ChartTraning.js
@@ -30,7 +30,6 @@ const ChartTraning = ({ trainingData }) => {
   function getDaysArray(start, end) {
     const arr = [];
     for (const dt = new Date(start); dt <= new Date(end); dt.setDate(dt.getDate() + 1)) {
-      const newDate = new Date(dt)
       arr.push(formatDate(new Date(dt)));
     }
     return arr;
