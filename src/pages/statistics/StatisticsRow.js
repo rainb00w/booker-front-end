ae68f373aa6a7939875e0fc457a140f13ec6e357
jokerav <jokeravdeveloper@gmail.com>
2022-10-06 13:36:12 +0300

    получение даты и страниц с сервер
    получение даты и страниц с сервера

--- src/pages/statistics/StatisticsRow.js
+++ src/pages/statistics/StatisticsRow.js
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import s from './statisticsList.module.css';
 const StatisticsRow = ({ row }) => {
-  const { date, time, page } = row;
+  const { date, time, pages } = row;
   return (
     <li className={s.statisticsListItem}>
       <span className={s.cellDate}>{date}</span>
       <span className={s.cellTime}>{time}</span>
-      <span className={s.cellPage}>{page}</span>
+      <span className={s.cellPage}>{pages}</span>
     </li>
   );
 };
@@ -16,6 +16,6 @@ StatisticsRow.propTypes = {
   row: PropTypes.shape({
     date: PropTypes.string.isRequired,
     time: PropTypes.string.isRequired,
-    page: PropTypes.number.isRequired,
+    pages: PropTypes.number.isRequired,
   }).isRequired,
 };
--- src/pages/statistics/statisticsList.js
+++ src/pages/statistics/statisticsList.js
@@ -1,18 +1,49 @@
 import React from 'react';
 import StatisticsRow from './StatisticsRow';
 import s from './statisticsList.module.css';
+import { useGetAllTrainingsQuery } from '../../redux/books/trainingApi';
 const StatisticsList = () => {
-  const rows = [
-    { date: '10.10.2019', time: '08:08:00', page: 32 },
-    { date: '11.10.2019', time: '09:08:00', page: 154 },
-    { date: '12.10.2019', time: '10:08:00', page: 7 },
-    { date: '13.10.2019', time: '11:08:00', page: 12 },
-    { date: '14.10.2019', time: '12:08:00', page: 21 },
-  ];
+  Date.prototype.yyyymmdd = function () {
+    let mm = this.getMonth() + 1; // getMonth() is zero-based
+    let dd = this.getDate();
+
+    return [
+      this.getFullYear(),
+      (mm > 9 ? '' : '0') + mm,
+      (dd > 9 ? '' : '0') + dd,
+    ].join('-');
+  };
+  Date.prototype.hhmmss = function () {
+    let hh = this.getHours();
+    let mm = this.getMinutes();
+    return [(hh > 9 ? '' : '0') + hh, (mm > 9 ? '' : '0') + mm, '00'].join(':');
+  };
+
+  // const rows = [
+  //   { date: '10.10.2019', time: '08:08:00', pages: 32 },
+  //   { date: '11.10.2019', time: '09:08:00', pages: 154 },
+  //   { date: '12.10.2019', time: '10:08:00', pages: 7 },
+  //   { date: '13.10.2019', time: '11:08:00', pages: 12 },
+  //   { date: '14.10.2019', time: '12:08:00', pages: 21 },
+  // ];
+  const getAllTrainings = useGetAllTrainingsQuery();
+  const { results = [] } = getAllTrainings.data;
+  console.log(results, 'results');
+  let rows = [];
+  results.forEach(element => {
+    rows.push({
+      date: new Date(element.date).yyyymmdd(),
+      time: new Date(element.date).hhmmss(),
+      pages: element.pages,
+      id: element._id,
+    });
+  });
+
+  console.log(rows);
   return (
     <ul className={s.statisticsList}>
       {rows.map(row => (
-        <StatisticsRow key={row.date} row={row} className={s.statisticsList} />
+        <StatisticsRow key={row.id} row={row} className={s.statisticsList} />
       ))}
     </ul>
   );
