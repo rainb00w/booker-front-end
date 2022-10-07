95496c41aa26fcd499eaf06ca9934e54db0ce87d
Lilia Lomaka <llomaka80@gmail.com>
2022-10-07 21:30:57 +0300

    Adding opened eye color and disabling cut, copy and paste for password fields

--- src/components/pages/registration/registration.jsx
+++ src/components/pages/registration/registration.jsx
@@ -61,223 +61,233 @@ const Registration = () => {
     navigate('/login', { state: 'modal' });
   };
 
+  const disableChange = (event) => {
+    event.preventDefault();
+  };
+
 
   return (
     <>
       <section className={styles.section}>
         <div className={styles.left__block}>
-        <div className={styles.registr__form}>
-          <div className={styles.form__border}>
-            <Formik
-              initialValues={{
-                name: '',
-                email: '',
-                password: '',
-                confirmPassword: '',
-              }}
-              validationSchema={registrationValidationSchema}
-              onSubmit={(values, { resetForm }) => {
-                const { name, email, password } = values;
+          <div className={styles.registr__form}>
+            <div className={styles.form__border}>
+              <Formik
+                initialValues={{
+                  name: '',
+                  email: '',
+                  password: '',
+                  confirmPassword: '',
+                }}
+                validationSchema={registrationValidationSchema}
+                onSubmit={(values, { resetForm }) => {
+                  const { name, email, password } = values;
 
-                dispatch(authOperations.register({ name, email, password }))
-                  .then(answer => {
-                    const { data, response } = answer.payload;
+                  dispatch(authOperations.register({ name, email, password }))
+                    .then(answer => {
+                      const { data, response } = answer.payload;
 
-                    if (data) {
-                      resetForm({ values: '' });
-                      setErrName('');
-                      setErrEmail('');
-                      Notify.success(
-                        t(`notify_phrase1`)
-                      );
-                      setTimeout(() => {
-                        navigate('/login');
-                      }, 2000);
-                    } else if (response) {
-                      throw response.data.message;
-                    }
-                  })
-                  .catch(error => {
-                    switch (error) {
-                      case 'name':
-                        setErrName('this name is already taken, choose other');
-                        setErrEmail('');
-                        return;
-                      case 'email':
+                      if (data) {
+                        resetForm({ values: '' });
                         setErrName('');
-                        setErrEmail(
-                          'this email is already taken, choose other'
+                        setErrEmail('');
+                        Notify.success(
+                          t(`notify_phrase1`)
                         );
-                        return;
-                      case 'name&email':
-                        setErrName('this name is already taken, choose other');
-                        setErrEmail(
-                          'this email is already taken, choose other'
-                        );
-                        return;
-                      default:
-                        return;
-                    }
-                  });
-              }}
-            >
-              {({
-                values,
-                errors,
-                touched,
-                handleBlur,
-                handleChange,
-                handleSubmit,
-              }) => (
-                <form onSubmit={handleSubmit}>
-                  <a
-                    className={styles.google__auth}
-                    href="https://booker-back-end.herokuapp.com/api/user/google"
-                  >
-                    <div className={styles.google__container}>
-                      <svg className={styles.svg__google}>
-                        <use href={svgPath.google + '#google'}></use>
-                      </svg>
-                      <p className={styles.google__text}>Google</p>
-                    </div>
-                  </a>
-                  <p className={styles.label__title}>
-                    {t('name')}
-                    <span className={styles.label__star}>*</span>
-                    {errName && (
-                      <span className={styles.error}>{t(`${errName}`)}</span>
-                    )}
-                  </p>
-                  <input
-                    className={styles.input}
-                    type="text"
-                    placeholder="Name"
-                    name="name"
-                    value={values.name}
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                  />
-                  {errors.name && touched.name ? (
-                    <p className={styles.warning}>{t(`${errors.name}`)}</p>
-                  ) : (
-                    <span className={styles.default__count}></span>
-                  )}
-                  <p className={styles.label__title}>
-                    {t('email')}
-                    <span className={styles.label__star}>*</span>
-                    {errEmail && (
-                      <span className={styles.error}>{t(`${errEmail}`)}</span>
-                    )}
-                  </p>
-                  <input
-                    className={styles.input}
-                    type="email"
-                    placeholder="your@email.com"
-                    name="email"
-                    value={values.email}
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                  />
-                  {errors.email && touched.email ? (
-                    <p className={styles.warning}>{t(`${errors.email}`)}</p>
-                  ) : (
-                    <span className={styles.default__count}></span>
-                  )}
-                  <label className={styles.label_password}>
+                        setTimeout(() => {
+                          navigate('/login');
+                        }, 2000);
+                      } else if (response) {
+                        throw response.data.message;
+                      }
+                    })
+                    .catch(error => {
+                      switch (error) {
+                        case 'name':
+                          setErrName('this name is already taken, choose other');
+                          setErrEmail('');
+                          return;
+                        case 'email':
+                          setErrName('');
+                          setErrEmail(
+                            'this email is already taken, choose other'
+                          );
+                          return;
+                        case 'name&email':
+                          setErrName('this name is already taken, choose other');
+                          setErrEmail(
+                            'this email is already taken, choose other'
+                          );
+                          return;
+                        default:
+                          return;
+                      }
+                    });
+                }}
+              >
+                {({
+                  values,
+                  errors,
+                  touched,
+                  handleBlur,
+                  handleChange,
+                  handleSubmit,
+                }) => (
+                  <form onSubmit={handleSubmit}>
+                    <a
+                      className={styles.google__auth}
+                      href="https://booker-back-end.herokuapp.com/api/user/google"
+                    >
+                      <div className={styles.google__container}>
+                        <svg className={styles.svg__google}>
+                          <use href={svgPath.google + '#google'}></use>
+                        </svg>
+                        <p className={styles.google__text}>Google</p>
+                      </div>
+                    </a>
                     <p className={styles.label__title}>
-                      {t('password')}
+                      {t('name')}
                       <span className={styles.label__star}>*</span>
+                      {errName && (
+                        <span className={styles.error}>{t(`${errName}`)}</span>
+                      )}
                     </p>
-                    <TextField
-                      required
-                      fullWidth
-                      autoComplete='new-password'
-                      type={showPassword ? 'text' : 'password'}
-                      name='password'
-                      inputProps={{ maxLength: 30 }}
-                      id={id + 'password'}
-                      placeholder='Password'
+                    <input
+                      className={styles.input}
+                      type="text"
+                      placeholder="Name"
+                      name="name"
+                      value={values.name}
                       onBlur={handleBlur}
                       onChange={handleChange}
-                      value={values.password}
-                      variant='standard'
-                      InputProps={{
-                        style: { fontFamily: "'Montserrat', sans-serif", },
-                        disableUnderline: true,
-                        endAdornment: <InputAdornment position="end">
-                          <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowPassword}
-                            onMouseDown={handleMouseDownPassword}
-                            edge="end"
-                          >
-                            {showPassword ? <Visibility /> : <VisibilityOff />}
-                          </IconButton>
-                        </InputAdornment>,
-                      }}
                     />
-                    {errors.password && touched.password ? (
-                      <p className={styles.warning}>
-                        {t(`${errors.password}`)}
-                      </p>
+                    {errors.name && touched.name ? (
+                      <p className={styles.warning}>{t(`${errors.name}`)}</p>
                     ) : (
                       <span className={styles.default__count}></span>
                     )}
-                  </label>
-                  <label className={styles.label_password}>
                     <p className={styles.label__title}>
-                      {t('confirmPassword')}
+                      {t('email')}
                       <span className={styles.label__star}>*</span>
+                      {errEmail && (
+                        <span className={styles.error}>{t(`${errEmail}`)}</span>
+                      )}
                     </p>
-                    <TextField
-                      required
-                      fullWidth
-                      autoComplete='new-password'
-                      type={showConfirm ? 'text' : 'password'}
-                      name='confirmPassword'
-                      inputProps={{ maxLength: 30 }}
-                      id={id + 'password'}
-                      placeholder='Password'
+                    <input
+                      className={styles.input}
+                      type="email"
+                      placeholder="your@email.com"
+                      name="email"
+                      value={values.email}
                       onBlur={handleBlur}
                       onChange={handleChange}
-                      value={values.confirmPassword}
-                      variant='standard'
-                      InputProps={{
-                        style: { fontFamily: "'Montserrat', sans-serif", },
-                        disableUnderline: true,
-                        endAdornment: <InputAdornment position="end">
-                          <IconButton
-                            aria-label="toggle password visibility"
-                            onClick={handleClickShowConfirm}
-                            onMouseDown={handleMouseDownConfirm}
-                            edge="end"
-                          >
-                            {showConfirm ? <Visibility /> : <VisibilityOff />}
-                          </IconButton>
-                        </InputAdornment>,
-                      }}
                     />
-                    {errors.confirmPassword && touched.confirmPassword ? (
-                      <p className={styles.warning}>
-                        {t(`${errors.confirmPassword}`)}
-                      </p>
+                    {errors.email && touched.email ? (
+                      <p className={styles.warning}>{t(`${errors.email}`)}</p>
                     ) : (
                       <span className={styles.default__count}></span>
                     )}
-                  </label>
-                  <button className={styles.form__button} type="submit">
-                    {t('signUp')}
-                  </button>
-                </form>
-              )}
-            </Formik>
-            <p className={styles.auth__describe}>
-              {t('alreadyHaveAnAccount')}
-              <Link className={styles.loginauth__link} to="/">
-                {t('login')}
-              </Link>
-            </p>
-          </div>
+                    <label className={styles.label_password}>
+                      <p className={styles.label__title}>
+                        {t('password')}
+                        <span className={styles.label__star}>*</span>
+                      </p>
+                      <TextField
+                        required
+                        fullWidth
+                        autoComplete='new-password'
+                        type={showPassword ? 'text' : 'password'}
+                        name='password'
+                        inputProps={{ maxLength: 30 }}
+                        id={id + 'password'}
+                        placeholder='Password'
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        onCut={disableChange}
+                        onCopy={disableChange}
+                        onPaste={disableChange}
+                        value={values.password}
+                        variant='standard'
+                        InputProps={{
+                          style: { fontFamily: "'Montserrat', sans-serif", },
+                          disableUnderline: true,
+                          endAdornment: <InputAdornment position="end">
+                            <IconButton
+                              aria-label="toggle password visibility"
+                              onClick={handleClickShowPassword}
+                              onMouseDown={handleMouseDownPassword}
+                              edge="end"
+                            >
+                              {showPassword ? <Visibility sx={{ color: '#FF6B08' }} /> : <VisibilityOff />}
+                            </IconButton>
+                          </InputAdornment>,
+                        }}
+                      />
+                      {errors.password && touched.password ? (
+                        <p className={styles.warning}>
+                          {t(`${errors.password}`)}
+                        </p>
+                      ) : (
+                        <span className={styles.default__count}></span>
+                      )}
+                    </label>
+                    <label className={styles.label_password}>
+                      <p className={styles.label__title}>
+                        {t('confirmPassword')}
+                        <span className={styles.label__star}>*</span>
+                      </p>
+                      <TextField
+                        required
+                        fullWidth
+                        autoComplete='new-password'
+                        type={showConfirm ? 'text' : 'password'}
+                        name='confirmPassword'
+                        inputProps={{ maxLength: 30 }}
+                        id={id + 'password'}
+                        placeholder='Password'
+                        onBlur={handleBlur}
+                        onChange={handleChange}
+                        onCut={disableChange}
+                        onCopy={disableChange}
+                        onPaste={disableChange}
+                        value={values.confirmPassword}
+                        variant='standard'
+                        InputProps={{
+                          style: { fontFamily: "'Montserrat', sans-serif", },
+                          disableUnderline: true,
+                          endAdornment: <InputAdornment position="end">
+                            <IconButton
+                              aria-label="toggle password visibility"
+                              onClick={handleClickShowConfirm}
+                              onMouseDown={handleMouseDownConfirm}
+                              edge="end"
+                            >
+                              {showConfirm ? <Visibility sx={{ color: '#FF6B08' }} /> : <VisibilityOff />}
+                            </IconButton>
+                          </InputAdornment>,
+                        }}
+                      />
+                      {errors.confirmPassword && touched.confirmPassword ? (
+                        <p className={styles.warning}>
+                          {t(`${errors.confirmPassword}`)}
+                        </p>
+                      ) : (
+                        <span className={styles.default__count}></span>
+                      )}
+                    </label>
+                    <button className={styles.form__button} type="submit">
+                      {t('signUp')}
+                    </button>
+                  </form>
+                )}
+              </Formik>
+              <p className={styles.auth__describe}>
+                {t('alreadyHaveAnAccount')}
+                <Link className={styles.loginauth__link} to="/">
+                  {t('login')}
+                </Link>
+              </p>
+            </div>
           </div>
         </div>
         {modal && (
