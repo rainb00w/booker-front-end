3fd7bdfb23e923d5563f9bed6ea443f0946d8e4b
puerarongo <akhardin96@gmail.com>
2022-10-07 23:54:41 +0300

    fix markup

--- src/pages/login/loginPage.jsx
+++ src/pages/login/loginPage.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Login from "../../components/pages/login/login";
+import LoginPhrase from '../../components/pages/login/loginPhrase';
+import svgPath from 'services/svgPath';
+import styles from "./loginPage.module.css";
 
 const LoginPage = () => {
     return (
-        <>
-            <Login />
-        </>
+        <section className={styles.section}>
+            <div className={styles.left__block}>
+                <Login />
+            </div>
+            <div className={styles.right__block}>
+                <div className={styles.log__text}>
+                    <svg className={styles.svg__qutation}>
+                        <use href={svgPath.quatation + '#quatation'}></use>
+                    </svg>
+                    <LoginPhrase />
+                </div>
+            </div>
+        </section>
     )
 };
 
