a7cf80dadebfba79bd9cdff9975a281d612f7e9e
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2022-10-08 18:33:08 +0300

    Revert "Merge branch 'main' of https://github.com/rainb00w/booker-front-end"
    This reverts commit db6ae0797d7636e0f9db8e046ce73354900a36c4, reversing
    changes made to 03b3810f8e5eeb9bdf3bf7f7f20783553ef16731.

--- src/components/bookAdd/bookAddForm.js
+++ src/components/bookAdd/bookAddForm.js
@@ -21,28 +21,28 @@ const BookAddForm = ({ handleClickClose, showAdd }) => {
     },
     validationSchema: Yup.object().shape({
       title: Yup.string()
-        .min(1, t('book_err_title1'))
-        .max(50, t('book_err_title2'))
-        .matches(/^[^\s-]/, t('book_err_title3'))
-        .required(t('book_err_title4')),
+        .min(1, 'Book name must be more than 1!')
+        .max(50, 'Book title should be less than 50')
+        .matches(/^[^\s-]/, 'Name should not start from space or dash')
+        .required('Book title is required'),
       author: Yup.string()
-        .min(1, t('book_err_author1'))
-        .max(50, t('book_err_author2'))
-        .matches(/^[^\s-]/, t('book_err_author3'))
+        .min(1, 'Author name must be more than 1!')
+        .max(50, 'Author name should be less than 50')
+        .matches(/^[^\s-]/, 'Name should not start from space or dash')
         .matches(
           /^[a-zA-Zа-яА-ЯіІїЇєЄ]+(([' -][a-zA-Zа-яА-ЯіІїЇєЄ.])?[a-zA-Zа-яА-ЯіІїЇєЄ.]*)*$/,
-          t('book_err_author4')
+          'THIS FIELD CANNOT CONTAIN NUMBERS'
         )
-        .required(t('book_err_author5')),
+        .required('Author is required'),
       year: Yup.number()
-        .typeError(t('book_err_year1'))
-        .min(1000, t('book_err_year2'))
-        .max(2022, t('book_err_year3')),
+        .typeError('Year should be a number')
+        .min(1000, 'Books have been publishing since 1000!')
+        .max(2022, 'Year should be less than currentYear!'),
       pages: Yup.number()
-        .typeError(t('book_err_pages1'))
-        .min(10, t('book_err_pages2'))
-        .max(9999, t('book_err_pages3'))
-        .required(t('book_err_pages4')),
+        .typeError('Pages should be a number')
+        .min(10, 'Too Short!')
+        .max(9999, 'Must be no more than 4 characters')
+        .required('Pages is required'),
     }),
     onSubmit: async ({ title, author, year, pages }, { resetForm }) => {
       if (year === '') {
--- src/pages/statistics/sendPageForm.js
+++ src/pages/statistics/sendPageForm.js
@@ -39,14 +39,14 @@ const SendPageForm = ({ startDate = null }) => {
       dateInput: Yup.date()
         .min(
           new Date(minDate).yyyymmdd(),
-          t('results_err1')
+          'Ви не можете ввести дату до початку тренування'
         )
-        .max(new Date(today).yyyymmdd(), t('results_err2')),
+        .max(new Date(today).yyyymmdd(), 'Ви не можете ввести цю дату'),
       pageInput: Yup.number()
-        .positive(t('results_err3'))
-        .integer(t('results_err4'))
-        .max(999, t('results_err5'))
-        .required(t('results_err6')),
+        .positive('Введіть корректну кількість сторінок')
+        .integer('К-ть сторінок має бути ціла')
+        .max(999, 'Забагато сторінок')
+        .required('Введіть кількість сторінок'),
     }),
     onSubmit: ({ dateInput, pageInput }, { resetForm }) => {
       let dateToSend = new Date();
@@ -103,7 +103,7 @@ const SendPageForm = ({ startDate = null }) => {
             <div>{formik.errors.pageInput}</div>
           ) : null}
         </label>
-
+    
       </div>
       {error && <div className={s.backEndError} ><p>{error.data.message}</p></div>}
       <button className={s.addResultBtn} type="submit">
